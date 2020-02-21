28df74235dc53d644e34a6ee7919e94deab8598b
travis-vela <52721388+travis-vela@users.noreply.github.com>
2020-02-21 10:44:25 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>hello form this line in  the react project</p>
+        <h1>hello!!</h1>
+        <h3>good bye bye ():(</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
